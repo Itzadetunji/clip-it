4a491bdb13946da903af85a8da57295a254ff59a
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2026-03-17 12:24:08 +0100

    Initial Commit

---
+++ Screen Record/Screen_RecordApp.swift
@@ -0,0 +1,32 @@
+//
+//  Screen_RecordApp.swift
+//  Screen Record
+//
+//  Created by Adetunji Adeyinka on 17/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Screen_RecordApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
